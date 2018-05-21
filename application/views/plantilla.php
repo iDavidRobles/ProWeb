dc2aac4fe4f502015d7a63f1632c5fc63e96b185
JRios112 <jorgerios112@hotmail.com>
2018-05-20 17:43:46 -0700

    actualizacion de logo

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -8,7 +8,9 @@
 
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">ProvWeb</a>
+      <a class="navbar-brand" href="#">
+        <img src="/assets/imagenes/logo.png" alt="" width="100px" height="40px">
+      </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
