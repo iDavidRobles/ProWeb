907c84df70871677939644e7826b084d894aec4f
David Robles <xroblesburgos@gmai.com>
2018-05-20 17:23:44 -0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -1,1 +1,34 @@
-login logout
+
+  <style>
+  body{
+	background: #eee !important;
+  }
+  .container-fluid{
+    max-width: 380px;
+    padding: 15px 35px 45px;
+    margin: 0 auto;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.1);
+  }
+  </style>
+  <br>
+  <br>
+  <br>
+  <br>
+    <div class="container-fluid">
+      <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Correo electronico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa correo">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Recuerdame</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Iniciar session</button>
+</form>
+</div>
